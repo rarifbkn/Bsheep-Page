dc4caa77487e3421685547f2e120f7cbf90a4507
rarifbkn <arif.bordones@gmail.com>
2025-01-27 20:22:50 -0300

    feat: Add Swiper Package and Carousel done

---
+++ src/utils/productsInfo.ts
@@ -0,0 +1,80 @@
+
+export const products_poleras = [
+    {
+        id: 1,
+        title: "Polera Negra",
+        description: "Polera manga corta color negro, hecha de algodon 100%",
+        price: "$11.990",
+        imageUrl: "/FotosPoleras/PoleraNegra.jpg",
+        sizes: ["S", "M", "L"],
+    },
+    {
+        id: 2,
+        title: "Polera Blanca",
+        description: "Polera manga corta color blanco, hecha de algodón 100%.",
+        price: "$11.990",
+        imageUrl: "/FotosPoleras/Foto Polera Blanca.jpg",
+        sizes: ["S", "M", "L"],
+    },
+    {
+        id: 3,
+        title: "Polera Cafe Oversize",
+        description: "Polera manga corta oversize hecha 100% algodon",
+        price: "$14.990",
+        imageUrl: "/FotosPoleras/Foto Polera Cafe.jpg",
+        sizes: ["S", "M", "L"],
+    },
+    
+    {
+        id: 6,
+        title: "Producto 5",
+        category: "Poleron",
+        description: "Descripción del producto 5",
+        price: "$14.990",
+        imageUrl: "/FotosPoleras/Foto Polera Blanca.jpg",
+        sizes: ["S", "M", "L"],
+    },
+    {
+        id: '123',
+        title: "Producto 6",
+        category: "Polera",
+        description: "Descripción del producto 6",
+        price: "$25.00",
+        imageUrl: "/FotosPoleras/Foto Polera Blanca.jpg",
+        sizes: ["S", "M", "L"],
+    },
+    {
+        id: '123',
+        title: "Producto 7",
+        category: "Polera",
+        description: "Descripción del producto 7",
+        price: "$25.00",
+        imageUrl: "/FotosPoleras/Foto Polera Blanca.jpg",
+        sizes: ["S", "M", "L"],
+    },
+    // Otros productos...
+];
+export const products_polerones = [
+    {
+        id: 4,
+        title: "Poleron Oversize Verde Botella",
+        description: "Poleron oversize 100% algodon ",
+        price: "$30.000",
+        imageUrl: "/FotosPolerones/1.jpg",
+        sizes: ["S", "M", "L"],
+    },
+    {
+        id: 5,
+        title: "Poleron Oversize Negro",
+        description: "Poleron Oversize hecho 100% algodon ",
+        price: "$30.000",
+        imageUrl: "/FotosPolerones/2.jpg",
+        sizes: ["S", "M", "L"],
+    },
+]
+export const secciones = [
+    { name: "INICIO", url: "/#rusel", label: "Inicio" },
+    { name: "PRODUCTOS", url: "/#products", label: "Productos" },
+    { name: "GUIA DE TALLAS", url: "#" },
+    { name: "CONTACTO", url: "mailto:bsheep.cl@gmail.com", label: "Contacto", },
+];
