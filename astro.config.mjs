959487f8d53335b2f65cd06044a158c26ddcf09f
rarifbkn <arif.bordones@gmail.com>
2025-02-28 12:57:32 -0300

    refactor(netlify): try netlify deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,14 @@ import preact from '@astrojs/preact';
 
 import db from "@astrojs/db";
 
-import vercel from '@astrojs/vercel';
+
+import netlify from '@astrojs/netlify';
+
 
 // https://astro.build/config
 export default defineConfig({
   site:"https://bsheep.netlify.app/",
   integrations: [preact(), db()],
   output: 'server',
-  adapter: vercel(),
+  adapter: netlify(),
 });
